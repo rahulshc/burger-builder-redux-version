968e8d59fddae43729b8c10e3292347bf41f7f9c
Swastika Library <rahul1.shc@gmail.com>
2020-01-28 13:41:03 +0530

    Backing up on Github

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,194 @@
+import React, {Component} from 'react';
+import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.module.css';
+import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
+
+class ContactData extends Component {
+    state = {
+        orderForm: {
+           name: {
+               elementType: 'input',
+               elementConfig: {
+                   type: 'text',
+                   placeholder: 'Your Name'
+               },
+               value: '',
+               validation :{
+                   required: true
+               },
+               valid: false,
+               touched: false
+           },
+           street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: '',
+                validation :{
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+           zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: '',
+                validation :{
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false,
+                touched: false
+            },
+           country: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'text',
+                placeholder: 'Country'
+            },
+            value: '',
+            validation :{
+                required: true
+            },
+            valid: false,
+            touched: false
+        },
+           email: {
+            elementType: 'input',
+            elementConfig: {
+                type: 'email',
+                placeholder: 'Your Mail'
+            },
+            value: '',
+            validation :{
+                required: true
+            },
+            valid: false,
+            touched: false
+        },
+           deliveryMethod: {
+            elementType: 'select',
+            elementConfig: {
+                options: [
+                    {value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}
+                ]
+            },
+            //default value of '' will create issue when submitting the form without changing the dropdown emppty value wil be sent to server
+            value: '',
+            valid: true,
+            validation: {}
+            
+        }
+        },
+        formIsValid: false,
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true});
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+        const order ={
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData: formData
+        }
+        axios.post('/orders.json', order).then(response => {
+            this.setState({loading: false});
+            this.props.history.push('/');
+        })
+        .catch(error => {
+            this.setState({loading: false});
+        });
+       
+    }
+
+    checkValidity (value, rules) {
+        let isValid = true;
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+        if(rules.minLength) {
+            isValid=value.length >= rules.minLength && isValid;
+        }
+        if(rules.maxLength) {
+            isValid=value.length <= rules.maxLength && isValid
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        }
+        //we do not want to mutate state directly that is why we are cloning objects firsts
+        updatedFormElement.value=event.target.value;
+        //if check below required otherwise it will give can not find 'valid' error for select element onchange
+        if('valid' in updatedFormElement) updatedFormElement.valid=this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        if('touched' in updatedFormElement) updatedFormElement.touched=true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm){
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        //console.log(updatedFormElement);
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
+    }
+
+    render () {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+        let form=(
+            <form onSubmit={this.orderHandler}>
+                    {formElementsArray.map(formElement => (
+                        <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
+                    ))}
+                    <Button btnType='Success' disabled={!this.state.formIsValid}>ORDER</Button>
+                </form>
+        );
+        if(this.state.loading){
+            form = <Spinner />;
+        }
+        return(
+            <div className={classes.ContactData}>
+                <h4>Enter Your Contact Data</h4>
+                {form}
+            </div>
+        );
+    }
+
+}
+
+export default ContactData;
